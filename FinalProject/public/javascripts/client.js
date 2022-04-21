31bc74d6bc8940ec7d54764120ebbbc01be23111
Jamie Youn <jyoun@nd.edu>
2022-04-21 12:32:07 -0400

    changes

--- FinalProject/public/javascripts/client.js
+++ FinalProject/public/javascripts/client.js
@@ -60,6 +60,38 @@ stadiumButton.addEventListener('click', function(e) {
   //document.getElementById("p1").innerHTML = "Chloe Cow";  
 });
 
+const likeButton = document.getElementById('likeButton');
+likeButton.addEventListener('click', function(e) {
+    console.log('Like button was clicked');
+
+    fetch('/clicked', {method: 'POST'})
+        .then(function(response) {
+            if(response.ok) {
+                console.log('Click was recorded');
+                return;
+            }
+            throw new Error('Request failed.');
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
+
+setInterval(function() {
+    fetch('/clicks', {method: 'GET'})
+        .then(function(response) {
+            if(response.ok) return response.json();
+            throw new Error('Request failed.');
+        })
+        .then(function(data) {
+            document.getElementById('counter').innerHTML = `Button was clicked ${data.length} times`;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}, 1000);
+
+/*
 var map;
 
 function initMap() {
@@ -112,7 +144,7 @@ function initMap() {
      label: { color: '#f7f7f7', fontWeight: 'bold', fontSize: '10px', text: 'Crocodiles' },
      map: map
    });
-
 }
+*/
 
 
