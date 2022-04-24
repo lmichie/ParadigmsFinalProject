56ca8c8522d84f2283b0c2019db6cf0c30a2c356
Lindsey Michie <69827958+lmichie@users.noreply.github.com>
2022-04-24 13:01:41 -0400

    moved quiz questions

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -9,56 +9,6 @@ var likesRouter = require('./routes/likes');
 
 var app = express();
 
-//Quiz set up
-const questions = [
-  {
-    question: "Who is the President of Notre Dame?",
-    answers: {
-      a: "Joe Biden",
-      b: "Knute Rockne",
-      c: "Father John Jenkins"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "What year was the Universoty founded?",
-    answers: {
-      a: "1842",
-      b: "1953",
-      c: "1776"
-    },
-    correctAnswer: "a"
-  },
-  {
-    question: "Notre Dame is located in what city?",
-    answers: {
-      a: "New York",
-      b: "Chicago",
-      c: "Indianpolis",
-      d: "South Bend"
-    },
-    correctAnswer: "d"
-  },
-  {
-    question: "How many residence halls are on campus?",
-    answers: {
-      a: "51",
-      b: "32",
-      c: "28"
-    },
-    correctAnswer: "b"
-  },{
-    question: "What are Notre Dame's offical colors?",
-    answers: {
-      a: "Blue and Gold",
-      b: "Green and Navy",
-      c: "Gold and Green"
-    },
-    correctAnswer: "a"
-  }
- ];
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
