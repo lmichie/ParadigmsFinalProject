cea4e915e75e023a08740b899d72dde8ba6801b5
Lindsey Michie <69827958+lmichie@users.noreply.github.com>
2022-04-24 13:08:24 -0400

    Update index.js

--- FinalProject/controllers/index.js
+++ FinalProject/controllers/index.js
@@ -1,3 +1,52 @@
+//Quiz questions
+const questions = [
+  {
+    question: "Who is the President of Notre Dame?",
+    answers: {
+      a: "Joe Biden",
+      b: "Knute Rockne",
+      c: "Father John Jenkins"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What year was the Universoty founded?",
+    answers: {
+      a: "1842",
+      b: "1953",
+      c: "1776"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Notre Dame is located in what city?",
+    answers: {
+      a: "New York",
+      b: "Chicago",
+      c: "Indianpolis",
+      d: "South Bend"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "How many residence halls are on campus?",
+    answers: {
+      a: "51",
+      b: "32",
+      c: "28"
+    },
+    correctAnswer: "b"
+  },{
+    question: "What are Notre Dame's offical colors?",
+    answers: {
+      a: "Blue and Gold",
+      b: "Green and Navy",
+      c: "Gold and Green"
+    },
+    correctAnswer: "a"
+  }
+ ];
+
 exports.index = function(req, res, next) {
   res.render('index', { about: 'Welcome to our campus!' , location_name:
  'Campus Map'});
@@ -31,6 +80,6 @@ exports.grotto = function(req, res, next) {
 }
 
 exports.quiz = function(req, res, next) {
-   res.render('quiz', { markers: 'Markers will go here.'}); 
+   res.render('quiz', {questions}); 
 }
 
