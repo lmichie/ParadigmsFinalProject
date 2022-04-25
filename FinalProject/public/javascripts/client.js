0f75e2d24b52defe6516fd82a5c1102d9e6d522a
Lindsey Michie <69827958+lmichie@users.noreply.github.com>
2022-04-24 20:41:01 -0400

    Update client.js

--- FinalProject/public/javascripts/client.js
+++ FinalProject/public/javascripts/client.js
@@ -55,6 +55,53 @@ startButton.addEventListener('click', function(e) {
   console.log('Start button was clicked');
   buildQuiz();
 });
+const questions = [
+  {
+    question: "Who is the President of Notre Dame?",
+    answers: {
+      a: "Joe Biden",
+      b: "Knute Rockne",
+      c: "Father John Jenkins"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What year was the Universoty founded?",
+    answers: {
+      a: "1842",
+      b: "1953",
+      c: "1776"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Notre Dame is located in what city?",
+    answers: {
+      a: "New York",
+      b: "Chicago",
+      c: "Indianpolis",
+      d: "South Bend"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "How many residence halls are on campus?",
+    answers: {
+      a: "51",
+      b: "32",
+      c: "28"
+    },
+    correctAnswer: "b"
+  },{
+    question: "What are Notre Dame's offical colors?",
+    answers: {
+      a: "Blue and Gold",
+      b: "Green and Navy",
+      c: "Gold and Green"
+    },
+    correctAnswer: "a"
+  }
+ ];
 
 const likeButton = document.getElementById('likeButton');
 likeButton.addEventListener('click', function(e) {
