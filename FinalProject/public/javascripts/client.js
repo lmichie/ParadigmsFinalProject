0e93add0f2355b59ae08d5ba1964011f2eb7ab56
Lindsey Michie <69827958+lmichie@users.noreply.github.com>
2022-04-26 13:50:03 -0400

    globals

--- FinalProject/public/javascripts/client.js
+++ FinalProject/public/javascripts/client.js
@@ -6,6 +6,56 @@ console.log('Client-side code running');
 // This is Prof. Huang's port.  Use your own please!!
 const myURL = "http://student04.cse.nd.edu:51036";
 
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const myQuestions = [
+  {
+    question : "Who is the President of Notre Dame?",
+    answers: {
+      a: "Joe Biden",
+      b: "Knute Rockne",
+      c: "Father John Jenkins"
+    },
+    correctAnswer: "c"
+  },
+  {
+     question: "What year was the Universoty founded?",
+     answers: {
+       a: "1842",
+       b: "1953",
+       c: "1776"
+     },
+     correctAnswer: "a"
+  },
+  {
+    question: "Notre Dame is located in what city?",
+    answers: {
+      a: "Chicago",
+      b: "Indianpolis",
+      c: "South Bend"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question : "How many residence halls are on campus?",
+    answers : {
+      a: "51",
+      b: "32",
+      c: "28"
+    },
+    correctAnswer : "b"
+  },
+  {
+    question : "What are Notre Dame's offical colors?",
+    answers : {
+      a: "Blue and Gold",
+      b: "Green and Navy",
+      c: "Gold and Green"
+    },
+    correctAnswer: "a"
+  }
+];
+
 const overviewButton = document.getElementById('overviewButton');
 overviewButton.addEventListener('click', function(e) {
   console.log('Overview button was clicked');
@@ -70,8 +120,6 @@ const submitButton = document.getElementById('submitButton');
 if (submitButton) {
   submitButton.addEventListener('click', function(e) {
     console.log('submit button was clicked');
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
     const answerContainers = quizContainer.querySelectorAll('.answers');
     let numCorrect = 0;
     myQuestions.forEach( (currentQuestion, questionNumber) => {
@@ -94,55 +142,6 @@ const startButton = document.getElementById('startButton');
 if (startButton) {
   startButton.addEventListener('click', function(e) {
     console.log('Start button was clicked');
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const myQuestions = [
-        {
-            question : "Who is the President of Notre Dame?",
-            answers: {
-                a: "Joe Biden",
-                b: "Knute Rockne",
-                c: "Father John Jenkins"
-            },
-            correctAnswer: "c"
-        },
-        {
-            question: "What year was the Universoty founded?",
-            answers: {
-                a: "1842",
-                b: "1953",
-                c: "1776"
-            },
-            correctAnswer: "a"
-        },
-        {
-            question: "Notre Dame is located in what city?",
-            answers: {
-                a: "Chicago",
-                b: "Indianpolis",
-                c: "South Bend"
-            },
-            "correctAnswer": "c"
-        },
-        {
-            question : "How many residence halls are on campus?",
-            answers : {
-               a: "51",
-               b: "32",
-               c: "28"
-            },
-            "correctAnswer" : "b"
-        },
-        {
-            question : "What are Notre Dame's offical colors?",
-            answers : {
-               a: "Blue and Gold",
-               b: "Green and Navy",
-               c: "Gold and Green"
-            },
-            correctAnswer: "a"
-        }
-    ];
     const output = [];
     myQuestions.forEach(
       (currentQuestion, questionNumber) => {
